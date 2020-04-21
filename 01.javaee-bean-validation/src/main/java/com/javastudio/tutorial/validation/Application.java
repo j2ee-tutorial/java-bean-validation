3736b16c69d6c303411875f36c06f148abfaca6a
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-04-21 08:02:04 +0430

    javaee-bean-validation

---
+++ 01.javaee-bean-validation/src/main/java/com/javastudio/tutorial/validation/Application.java
@@ -0,0 +1,12 @@
+package com.javastudio.tutorial.validation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Application started!");
+    }
+}
