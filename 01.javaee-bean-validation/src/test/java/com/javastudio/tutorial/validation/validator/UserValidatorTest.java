1608c9cfd76936f9f1721fc45c166535bbd7ba4e
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-04-21 11:10:59 +0430

    Bean Validation

--- 01.javaee-bean-validation/src/test/java/com/javastudio/tutorial/validation/validator/UserValidatorTest.java
+++ 01.javaee-bean-validation/src/test/java/com/javastudio/tutorial/validation/validator/UserValidatorTest.java
@@ -1,7 +1,7 @@
 package com.javastudio.tutorial.validation.validator;
 
 import com.javastudio.tutorial.validation.model.User;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,32 +12,33 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserValidatorTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserValidatorTest.class);
 
     @Test
     void validateUser() {
-        //Create ValidatorFactory which returns validator
+        // Create ValidatorFactory which returns validator
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 
-        //It validates bean instances
+        // It validates bean instances
         Validator validator = factory.getValidator();
 
         User user = new User(null, "1", "abcgmail.com");
 
-        //Validate bean
+        // When validate bean
         Set<ConstraintViolation<User>> constraintViolations = validator.validate(user);
 
-        //Show errors
+        // Show errors
         if (constraintViolations.size() > 0) {
-            for (ConstraintViolation<User> violation : constraintViolations) {
-                LOGGER.info(violation.getMessage());
-            }
+            constraintViolations.stream().map(ConstraintViolation::getMessage).forEach(LOGGER::info);
         } else {
             LOGGER.info("Valid Object");
         }
+
+        // Then
+        Assertions.assertThat(constraintViolations)
+                .extracting(ConstraintViolation::getMessage)
+                .contains("Invalid Username", "Invalid Email");
     }
 }
